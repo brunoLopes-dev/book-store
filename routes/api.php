1b7c0de6ed3fb3a33e01ad6ccdf5d81a00c01f4a
Bruno <brunoslopes.dev@gmail.com>
2024-05-12 20:49:11 -0300

    feat add crud book and store

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\StoreController;
 
+// Rota para login
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Grupo de rotas que requerem autenticação
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/books', BookController::class);
+    Route::apiResource('/stores', StoreController::class);
+    Route::post('/stores/{store}/books/{book}', [StoreController::class, 'attachBook']);
+});
 
